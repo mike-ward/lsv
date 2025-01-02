b2345f7179ecf95f004e93ae4f9eac6a1f81bb29
mike-ward <mike@mike-ward.net>
2025-01-02 14:55:11 -0600

    fix tests

--- lsv/natural_compare_test.v
+++ lsv/natural_compare_test.v
@@ -4,49 +4,49 @@ fn test_numbers_embdded_in_text() {
 	a := 'log10.txt'
 	b := 'log9.txt'
 
-	assert compare_strings(&b, &a) > 0
-	assert natural_compare(&b, &a) < 0
+	assert string_compare(&b, &a, false) > 0
+	assert natural_compare(&b, &a, false) < 0
 
-	assert compare_strings(&a, &b) < 0
-	assert natural_compare(&a, &b) > 0
+	assert string_compare(&a, &b, false) < 0
+	assert natural_compare(&a, &b, false) > 0
 
-	assert compare_strings(&a, &a) == 0
-	assert natural_compare(&a, &a) == 0
+	assert string_compare(&a, &a, false) == 0
+	assert natural_compare(&a, &a, false) == 0
 
-	assert compare_strings(&b, &b) == 0
-	assert natural_compare(&b, &b) == 0
+	assert string_compare(&b, &b, false) == 0
+	assert natural_compare(&b, &b, false) == 0
 }
 
 fn test_numbers_two_embdded_in_text() {
 	a := '0log10.txt'
 	b := '1log9.txt'
 
-	assert compare_strings(&a, &b) < 0
-	assert natural_compare(&a, &b) < 0
+	assert string_compare(&a, &b, false) < 0
+	assert natural_compare(&a, &b, false) < 0
 
-	assert compare_strings(&b, &a) > 0
-	assert natural_compare(&b, &a) > 0
+	assert string_compare(&b, &a, false) > 0
+	assert natural_compare(&b, &a, false) > 0
 
-	assert compare_strings(&a, &a) == 0
-	assert natural_compare(&a, &a) == 0
+	assert string_compare(&a, &a, false) == 0
+	assert natural_compare(&b, &b, false) == 0
 
-	assert compare_strings(&b, &b) == 0
-	assert natural_compare(&b, &b) == 0
+	assert string_compare(&b, &b, false) == 0
+	assert natural_compare(&b, &b, false) == 0
 }
 
 fn test_no_numbers_in_text() {
 	a := 'abc'
 	b := 'bca'
 
-	assert compare_strings(&a, &b) < 0
-	assert natural_compare(&a, &b) < 0
+	assert string_compare(&a, &b, false) < 0
+	assert natural_compare(&a, &b, false) < 0
 
-	assert compare_strings(&b, &a) > 0
-	assert natural_compare(&b, &a) > 0
+	assert string_compare(&b, &a, false) > 0
+	assert natural_compare(&b, &a, false) > 0
 
-	assert compare_strings(&a, &a) == 0
-	assert natural_compare(&a, &a) == 0
+	assert string_compare(&a, &a, false) == 0
+	assert natural_compare(&b, &b, false) == 0
 
-	assert compare_strings(&b, &b) == 0
-	assert natural_compare(&b, &b) == 0
+	assert string_compare(&b, &b, false) == 0
+	assert natural_compare(&b, &b, false) == 0
 }
