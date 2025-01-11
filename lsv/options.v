995b759a44984b828dbb9e96099cd1662af21207
mike-ward <mike@mike-ward.net>
2025-01-11 11:10:41 -0600

    add some more icons

--- lsv/options.v
+++ lsv/options.v
@@ -92,7 +92,7 @@ fn parse_args(args []string) Options {
 	mut fp := flag.new_flag_parser(args)
 
 	fp.application(app_name)
-	fp.version('v2025.2')
+	fp.version('v2025.3_A')
 	fp.skip_executable()
 	fp.description('List information about FILES')
 	fp.arguments_description('[FILES]')
